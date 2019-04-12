610bf7732c213a38bf16f6f044966b609792a726
Shane678 <44868357+Shane678@users.noreply.github.com>
2019-04-12 14:01:58 +0800

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -1,11 +1,11 @@
-/*
+ï»¿/*
 Hello.java
 2019-04-08
 v1.0
 */
 public class Hello{
 	public static void main(String[] args){
-		//Êä³ö¿ØÖÆÌ¨ĞÅÏ¢
+		//è¾“å‡ºæ§åˆ¶å°ä¿¡æ¯
 		System.out.print("hello world!\t");	
 		System.out.print("hello world2!");
 	}
